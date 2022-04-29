fc14fb22324a91a72c76160b8da2f4f125739a32
HenwinC <Henwincuff@gmail.com>
2022-04-29 15:33:58 -0500

    updated fixed

--- index.js
+++ index.js
@@ -1,72 +1,183 @@
 const express = require("express");
 
-const server = express();
+// create an express server from the express function above
+const server = express(); // this server is deaf AF. Can't hear ANYTHING. It's locked out of the world
 
+// Tell our server how to process different payloads
 server.use(express.json());
-// let PORT;
-const PORT = process.env.PORT||3000;
 
+const PORT = process.env.PORT || 3000;
+
+// Make the server listen on a port (on our computer)
 server.listen(PORT, () => {
-  console.log("Server listening")
+  console.log("Server listening...");
 });
 
-// create => post
-// read => get
-// update => put
-// delete => delete
+// CRUD
+// CREATE => POST
+// READ => GET
+// UPDATE => PUT
+// DELETE => DELETE
+
 const destinations = [];
 
 const students = {
-  bobby:{
-    name:"bobby",
-    intrest:"volleyball"
+  henwin: {
+    name: "Henwin",
+    interests: ["football"],
+    city: "Austin",
   },
-  sally:{
-    name:"sally",
-    intrest:"soccer"
+  dao: {
+    name: "Dao",
+    interests: ["tacos"],
+    city: "Sac Town",
   },
-  yurt:{
-    name:"yurt",
-    intrest:"football"
+  nikko: {
+    name: "Nikko",
+    interests: ["bananas"],
+    city: "Detroit",
+  },
+  will: {
+    name: "Will",
+    interests: ["camarro", "frontier", "wrangler", "bananas"],
+    city: "Detroit",
+  },
+  mannie: {
+    name: "Mannie",
+    interests: ["soccer", "bananas"],
+    city: "Georgia",
+  },
+};
+
+// GET /students
+// ?name=STUDENT_NAME
+// ?interest
+// ?city
+// localhost:3000/students
+// localhost:3000/students?name=nikko
+// localhost:3000/students?city=detroit&interest=banana
+server.get("/students", (req, res) => {
+  const { name, interest, city } = req.query;
+
+  if (!name && !interest && !city) {
+    return res.send(students);
   }
-}
-let filteredStudents = Object.values(students)
 
-server.get("/students", (req,res) => {
-  const {name , intrest} = req.query;
-  let filteredStudents = Object.values(students)
-  if(intrest){
-    filteredStudents = filteredStudents.filter((student) => student.intrest.includes(intrest.toLowerCase()))
+  if (name) {
+    const student = students[name.toLowerCase()];
+
+    if (student) {
+      return res.send(student);
+    }
+
+    return res
+      .status(404)
+      .send({ error: `Student by the name of ${name} not found` });
   }
-  if (name){
-    const students = students[name.toLowerCase()];
+
+  let filteredStudents = Object.values(students);
+
+  if (interest) {
+    filteredStudents = filteredStudents.filter((student) =>
+      student.interests.includes(interest.toLowerCase())
+    );
   }
-  if (student) {
-    return res.send(student);
+
+  if (city) {
+    filteredStudents = filteredStudents.filter(
+      (student) => student.city.toLowerCase() === city.toLowerCase()
+    );
   }
- return res.send(filteredStudents)
-})
 
-server.post("/destinations", (req,res) =>{
-const {destination, location, photo,description} = req.body
+  return res.send(filteredStudents);
+});
 
-if(!destination || !location || destination.length == 0|| location.length ==0){
-  return res.status(400).send({error: "Destination and location is required"})
-}
-if (photo && photo.length !==0){
+// GET /students/city/:myCity
+// USING NAMED ROUTE PARAMETERS
+// localhost:3000/students/city/detroit
+/*
+  {
+    myCity: detroit
+  }
+*/
 
-}
-const newDest ={
-  destination,
-  location,
-  photo: photo && photo.length !==0 ? photo : "yurt",
-  description: description && description.length !==0 ? description : "",
-}
+server.get("/students/name/:name", (req, res) => {
+  const { name } = req.params;
 
- destinations.push(newDest)
- res.redirect(303,"/destinations")
-})
+  if (name) {
+    const student = students[name.toLowerCase()];
 
-server.get("/destinations", (req,res)=> {
+    if (student) {
+      return res.send(student);
+    }
+
+    return res
+      .status(404)
+      .send({ error: `Student by the name of ${name} not found` });
+  }
+});
+server.get("/students/city/:city", (req, res) => {
+  const { city } = req.params;
+
+  if (city) {
+    const filteredStudents = Object.values(students).filter(
+      (student) => student.city.toLowerCase() === city.toLowerCase()
+    );
+
+    return res.send(filteredStudents);
+  }
+});
+server.get("/students/interest/:interest", (req, res) => {
+  const { interest } = req.params;
+
+  if (interest) {
+    const filteredStudents = Object.values(students).filter((student) =>
+      student.interests.includes(interest.toLowerCase())
+    );
+
+    return res.send(filteredStudents);
+  }
+});
+
+// CREATE => POST
+// POST /destinations
+// What is a destination? What makes a destination record?
+/*
+  - destination name (REQUIRED)
+  - location (REQUIRED)
+  - photo
+  - description
+*/
+server.post("/destinations", (req, res) => {
+  // ONLY grab what I need
+  const { destination, location, photo, description } = req.body;
+
+  // VALIDATE that I got what I expected (i.e destination & location are BOTH present and NOT empty strings)
+  if (
+    !destination ||
+    !location ||
+    destination.length === 0 ||
+    location.length === 0
+  ) {
+    return res
+      .status(400)
+      .send({ error: "Destination AND location are BOTH required" });
+  }
+
+  // Create the new object to put in my DB
+  const newDest = {
+    destination,
+    location,
+    photo: photo && photo.length !== 0 ? photo : "kakdjkajkdjkajkdj",
+    description: description ? description : "",
+  };
+
+  destinations.push(newDest);
+
+  res.redirect(303, "/destinations"); // => go to a GET /destinations
+});
+
+// GET /destinations
+server.get("/destinations", (req, res) => {
   res.send(destinations);
 });

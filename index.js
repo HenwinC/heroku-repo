4a781f5d6a85795d0f41a7e3ec68541d39657ab4
HenwinC <Henwincuff@gmail.com>
2022-04-29 12:14:30 -0500

    updated

--- index.js
+++ index.js
@@ -1,13 +1,72 @@
-const express = require("express")
+const express = require("express");
 
-const server = express()
+const server = express();
 
-const PORT = 3000
+server.use(express.json());
+// let PORT;
+const PORT = process.env.PORT||3000;
 
 server.listen(PORT, () => {
   console.log("Server listening")
 });
 
-server.get("/Henwin", (req,res)=> {
-  res.send("<h1> Hi Henwin!</h1>");
+// create => post
+// read => get
+// update => put
+// delete => delete
+const destinations = [];
+
+const students = {
+  bobby:{
+    name:"bobby",
+    intrest:"volleyball"
+  },
+  sally:{
+    name:"sally",
+    intrest:"soccer"
+  },
+  yurt:{
+    name:"yurt",
+    intrest:"football"
+  }
+}
+let filteredStudents = Object.values(students)
+
+server.get("/students", (req,res) => {
+  const {name , intrest} = req.query;
+  let filteredStudents = Object.values(students)
+  if(intrest){
+    filteredStudents = filteredStudents.filter((student) => student.intrest.includes(intrest.toLowerCase()))
+  }
+  if (name){
+    const students = students[name.toLowerCase()];
+  }
+  if (student) {
+    return res.send(student);
+  }
+ return res.send(filteredStudents)
+})
+
+server.post("/destinations", (req,res) =>{
+const {destination, location, photo,description} = req.body
+
+if(!destination || !location || destination.length == 0|| location.length ==0){
+  return res.status(400).send({error: "Destination and location is required"})
+}
+if (photo && photo.length !==0){
+
+}
+const newDest ={
+  destination,
+  location,
+  photo: photo && photo.length !==0 ? photo : "yurt",
+  description: description && description.length !==0 ? description : "",
+}
+
+ destinations.push(newDest)
+ res.redirect(303,"/destinations")
+})
+
+server.get("/destinations", (req,res)=> {
+  res.send(destinations);
 });
